5b9991834f93c2d810636faae9c76c0c142a3eec
gingerben93 <gingerben93@gmail.com>
2019-05-22 20:54:47 -0700

    fixes
    Small Fixes

---
+++ Poker GTO/Assets/Scripts/LoginScene/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Login : MonoBehaviour {
+
+    public const string LOGIN = "http://10.0.0.36:5000/get_free_cards";
+
+    //inputfields
+    public GameObject UserName;
+    public GameObject Password;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
